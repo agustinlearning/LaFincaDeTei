07d18514749c431f0df9d5ffe96eeeb1d04e6dfd
juanc <juanc@DESKTOP-C581ORR>
2026-07-21 17:23:53 -0400

    Maquetado de los paneles de Toros y Becerritos

--- LaFinca/src/visual/Principal.java
+++ LaFinca/src/visual/Principal.java
@@ -36,6 +36,7 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		String url = "imagenes_app/img";
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 834, 529);
 		
@@ -58,10 +59,30 @@ public class Principal extends JFrame {
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Listar");
 		mntmNewMenuItem_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				ListarVacas listarVaca = new ListarVacas();
+				listarVaca.setModal(true);
+				listarVaca.setVisible(true);
 			}
 		});
 		MenuVacas.add(mntmNewMenuItem_1);
+		
+		JMenu mnNewMenu = new JMenu("Toros");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem_2);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem_4);
+		
+		JMenu mnNewMenu_1 = new JMenu("Becerritos");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("New menu item");
+		mnNewMenu_1.add(mntmNewMenuItem_3);
+		
+		JMenuItem mntmNewMenuItem_5 = new JMenuItem("New menu item");
+		mnNewMenu_1.add(mntmNewMenuItem_5);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
