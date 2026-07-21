102e8f7b6cdf6ce82ebb9eed5a89459462715535
juanc <juanc@DESKTOP-C581ORR>
2026-07-21 17:22:16 -0400

    Añadidos getter y setters

--- LaFinca/src/logica/Toro.java
+++ LaFinca/src/logica/Toro.java
@@ -7,7 +7,11 @@ public class Toro extends Bovino {
 	ArrayList<Becerrito> misHijos;
 	public Toro(String nombre,String urlImagen, LocalDate fechaNac, String raza, String procedencia) {
 		super(nombre,urlImagen, fechaNac, raza, procedencia);
+		this.misHijos = new ArrayList<>();
 		// TODO Auto-generated constructor stub
+	}
+	public ArrayList<Becerrito> getMisHijos() {
+		return misHijos;
 	}
 
 }
